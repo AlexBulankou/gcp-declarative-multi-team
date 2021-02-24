76692e5b818fb91f7e9781b9bdd54af6668a3372
Alex Bulankou <alexbu@google.com>
2021-02-24 09:04:47 -0800

    Repo structure changes

--- environments/prod/shared/provision.tf
+++ environments/prod/shared/provision.tf
@@ -128,3 +128,23 @@ resource "google_service_account_iam_binding" "admin-account-iam" {
     google_service_account.cnrmsa
   ]
 }
+
+/*
+module "config_sync" {
+  source           = "terraform-google-modules/kubernetes-engine/google//modules/config-sync"
+
+  project_id       = var.project
+  cluster_name     = google_container_cluster.primary.name
+  location         = local.zone
+  cluster_endpoint = google_container_cluster.primary.endpoint
+  secret_type      = "none"
+
+  sync_repo        = "git@github.com:GoogleCloudPlatform/csp-config-management.git"
+  sync_branch      = "1.0.0"
+  policy_dir       = "foo-corp"
+
+  depends_on = [
+    google_container_cluster.primary
+  ]
+}
+*/
